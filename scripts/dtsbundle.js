1cfd21a980776faf2156750c499228beb0dfcca9
S. Chris Colbert <sccolbert@gmail.com>
2015-09-03 20:16:42 -0400

    update to TS 1.6

--- scripts/dtsbundle.js
+++
@@ -1,7 +0,0 @@
-require('dts-generator').generate({
-  name: 'phosphor-nodewrapper',
-  main: 'phosphor-nodewrapper/index',
-  baseDir: 'lib',
-  files: ['index.d.ts'],
-  out: 'lib/phosphor-nodewrapper.d.ts',
-});
